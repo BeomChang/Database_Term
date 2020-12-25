ed564de1d62a948abb4cd7b2d7edf5c143af659e
beomchangKim <fnzlvkdvkd@naver.com>
2020-12-26 01:24:30 +0900

    query test_3

--- search_product_page.php
+++ search_product_page.php
@@ -69,12 +69,11 @@
                 if($value != "") {
                     $query = $query . "$key = '".$value."'";
                     $opt_cnt--;
-                }
-                
-                if($opt_cnt != 0)
-                    $query = $query . "AND ";
-            }
 
+                    if($opt_cnt != 0)
+                        $query = $query . "AND ";
+                }
+            }
 
             // if($pid != "")
             //     $query = $query . "pid = '".$pname."'";
